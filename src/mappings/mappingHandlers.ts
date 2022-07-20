dbd8aa0796c885d0b8aa29f6e2b49d9dcb2e78d1
Andabakus <ivan@astar.network>
2022-07-20 22:32:09 +0200

    Playing around with SubQuery dApp Rewards

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -2,6 +2,7 @@ import { Option, Struct } from '@polkadot/types-codec';
 import { Balance } from '@polkadot/types/interfaces';
 import { SubstrateEvent } from "@subql/types";
 import { Tvl } from '../types';
+import { Reward } from '../types/models';
 
 export async function handleNewEraEvent(event: SubstrateEvent): Promise<void> {
     const {event: {data: [era]}} = event;
@@ -24,3 +25,21 @@ interface StakingEraInfo extends Struct {
     staked: Balance;
     locked: Balance;
 }
+
+
+export async function handleRewardEvent(event: SubstrateEvent): Promise<void> {
+
+    const eraIndex = (event.event.data[2]).toString();
+    const accountId = event.event.data[0].toString();
+    const idString = eraIndex + "___" + accountId
+    const record = new Reward(idString);
+
+    record.accountId = accountId
+    record.eraIndex = new Number(eraIndex).valueOf();
+    record.smartContract = event.event.data[1].toString();
+    record.balanceOf = (event.event.data[3] as Balance).toBigInt();
+
+    logger.info(`New event: ${record}`);
+    await record.save();
+}
+
